c6479987b70ed94e9962be9938653df613d7ed2d
Falk <julien.delusseau@gmail.com>
2020-09-17 12:00:18 +0200

    Test again

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express')
-const ConnectDB = require('./config/db')
+const connectDB = require('./config/db')
 const path = require('path')
-
-// Connect to database
-ConnectDB()
 
 const app = express()
 
-app.use(express.json({ extended: false }))
+// Connect Database
+connectDB()
 
+// Init Middleware
+app.use(express.json())
+
+// Define Routes
 app.use('/api/users', require('./routes/user'))
 
 // Serve static assets in production
@@ -22,6 +24,5 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => {
-  console.log('Server started')
-})
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
