750ca371fab40ba74b5955f99cb4576eda68026b
Falk <julien.delusseau@gmail.com>
2020-09-17 12:11:16 +0200

    Test

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,14 @@ import axios from 'axios'
 import { connect } from 'react-redux'
 import { loadUser } from './redux/actions/userActions'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-// import Landing from './pages/landing/Landing'
-// import Register from './pages/register/Register'
-// import Login from './pages/login/Login'
-// import Profiles from './pages/profiles/Profiles'
-// import PrivateRoute from './utils/PrivateRoute'
+import Landing from './pages/landing/Landing'
+import Register from './pages/register/Register'
+import Login from './pages/login/Login'
+import Profiles from './pages/profiles/Profiles'
+import PrivateRoute from './utils/PrivateRoute'
 // import AddProfile from './pages/addProfile/AddProfile'
 // import Movies from './pages/movies/Movies'
-// import Footer from './components/footer/Footer'
+import Footer from './components/footer/Footer'
 
 if (localStorage.netflixToken) {
   axios.defaults.headers.common['Authorization'] = localStorage.netflixToken
@@ -28,15 +28,14 @@ const App = ({ loadUser }) => {
   return (
     <Router>
       <Switch>
-        <Route exact path='/' component={() => <h1>Fucking heroku!</h1>} />
-        {/* <Route exact path='/' component={Landing} />
+        <Route exact path='/' component={Landing} />
         <Route exact path='/login' component={Login} />
         <Route exact path='/register' component={Register} />
         <PrivateRoute exact path='/profiles' component={Profiles} />
-        <PrivateRoute exact path='/add-profile' component={AddProfile} />
+        {/* <PrivateRoute exact path='/add-profile' component={AddProfile} />
         <PrivateRoute exact path='/movies' component={Movies} /> */}
       </Switch>
-      {/* <Footer /> */}
+      <Footer />
     </Router>
   )
 }
