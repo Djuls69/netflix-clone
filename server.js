b8ab91eee5a980ebf0935f13acb3b058249cb268
Falk <julien.delusseau@gmail.com>
2020-09-17 09:56:13 +0200

    Deployment to heroku

--- server.js
+++ server.js
@@ -16,19 +16,14 @@ app.use(cors())
 app.use('/api/users', require('./routes/user'))
 
 // Serve static assets in production
-app.use(express.static(path.join(__dirname, 'client/build')))
-
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')))
-  //
+  // Set static folder
+  app.use(express.static('client/build'))
+
   app.get('*', (req, res) => {
-    res.sendfile(path.join((__dirname = 'client/build/index.html')))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
-//build mode
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/public/index.html'))
-})
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
