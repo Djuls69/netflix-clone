aac23235a51537a3ea03c209533ceb4bb1575bc0
Falk <julien.delusseau@gmail.com>
2020-09-17 08:57:49 +0200

    Deployment to Heroku

--- server.js
+++ server.js
@@ -14,6 +14,21 @@ app.use(cors())
 
 app.use('/api/users', require('./routes/user'))
 
+// Serve static assets in production
+app.use(express.static(path.join(__dirname, 'client/build')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  //
+  app.get('*', (req, res) => {
+    res.sendfile(path.join((__dirname = 'client/build/index.html')))
+  })
+}
+//build mode
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/public/index.html'))
+})
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log('Server started')
